182d8972e7bc1554cbabb8e26cb0091f274bd442
rdavidm42 <drmayrhofer@gmail.com>
2026-06-29 17:35:20 -0500

    Updated the model for distilbert, reformatted several sections in notebook.

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
 from joblib import load
-from sklearn.feature_extraction.text import TfidfVectorizer
+from heapq import nlargest
 from recipe_preprocessing import preprocess_recipe
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from peft import PeftModel
+from wordcloud import WordCloud
 
 def categorize_recipe_svc(name, ingredients, instructions):
     with open("svc_recipes.pkl", "rb") as f:
@@ -14,10 +17,27 @@ def categorize_recipe_svc(name, ingredients, instructions):
     cleaned_text = preprocess_recipe(name, ingredients, instructions)
     return mlb.inverse_transform(model.predict([cleaned_text]))
 
+def feature_importance(term):
+        with open("svc_recipes.pkl", "rb") as f:
+            model = load(f)
+        with open("mlb.pkl", "rb") as f:
+            mlb = load(f)
+        feat_impts = [] 
+        for clf in model[1].estimators_:
+            feat_impts.append(clf.coef_)
+
+        classes_dictionary = dict(zip(mlb.classes_,np.arange(len(mlb.classes_))))
+        i = classes_dictionary[term.lower().replace(' ','-')]
+
+        features=dict(zip(model[0].get_feature_names_out(),feat_impts[i].flatten()))
+        # print(mlb.classes_[i])
+
+        wordcloud = WordCloud(relative_scaling=0,background_color='white').generate(' '.join(nlargest(25,features, key=features.get)))
+        return wordcloud
 @st.cache_resource
 def categorize_recipe_distilbert(name, ingredients, instructions):
     text = preprocess_recipe(name, ingredients, instructions)
-    path = '/Users/davidmayrhofer/Documents/Python/Machine Learning/Recipes Project/checkpoint-9480';
+    path = 'lora_weights';
     base_model_name = "distilbert-base-uncased"
     tokenizer = AutoTokenizer.from_pretrained(base_model_name)
     base_model = AutoModelForSequenceClassification.from_pretrained(base_model_name,
@@ -35,21 +55,36 @@ def categorize_recipe_distilbert(name, ingredients, instructions):
     return mlb.inverse_transform((logits>0).numpy().astype(int))
 def main():
     st.title("Recipe Categorizer")
-    st.write("Upload a recipe and let the model categorize it for you!")
-    recipe_name = st.text_input('Recipe Name')
-    recipe_ingredients = st.text_area('Recipe Ingredients (one per line)')
-    recipe_instructions = st.text_area('Recipe Instructions')
-    classifier = st.radio('Use',['Support Vector Classifier','Distilbert Neural Network'], horizontal=True)
-    if st.button('Categorize Recipe'):
-        if recipe_name and recipe_ingredients and recipe_instructions:
-            # Here you would call your model to categorize the recipe
-            if classifier == 'Support Vector Classifier':
-                category = list(categorize_recipe_svc(recipe_name, recipe_ingredients, recipe_instructions))
+    tab1, tab2 = st.tabs(["Classifier", "Explore the Model"])
+    with tab1:
+        st.write("Upload a recipe and let the model categorize it for you!")
+        recipe_name = st.text_input('Recipe Name')
+        recipe_ingredients = st.text_area('Recipe Ingredients (one per line)')
+        recipe_instructions = st.text_area('Recipe Instructions')
+        classifier = st.radio('Use',['Support Vector Classifier','Distilbert Neural Network'], horizontal=True)
+        if st.button('Categorize Recipe'):
+            if recipe_name and recipe_ingredients and recipe_instructions:
+                # Here you would call your model to categorize the recipe
+                if classifier == 'Support Vector Classifier':
+                    category = list(categorize_recipe_svc(recipe_name, recipe_ingredients, recipe_instructions))
+                else:
+                    category = list(categorize_recipe_distilbert(recipe_name, recipe_ingredients, recipe_instructions))
+                st.success(f'The recipe "{recipe_name}" has been categorized as: ' + ', '.join(category[0]).title().replace('-',' '))
             else:
-                category = list(categorize_recipe_distilbert(recipe_name, recipe_ingredients, recipe_instructions))
-            st.success(f'The recipe "{recipe_name}" has been categorized as: ' + ', '.join(category[0]).title().replace('-',' '))
-        else:
-            st.error('Please fill in all fields before categorizing.')
+                st.error('Please fill in all fields before categorizing.')
+    with tab2:
+        st.write("Explore what the model thinks are important words in each category")
+        with open("mlb.pkl", "rb") as f:
+            mlb = load(f)
+        tags = [x.title().replace('-',' ') for x in mlb.classes_]
+        selection = st.selectbox('Categories',tags)
+        # if st.button('Get Model Importance'):
+        wordcloud = feature_importance(selection)
+        fig = plt.figure()
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        plt.margins(x=0, y=0)
+        st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
--- recipe_preprocessing.py
+++ recipe_preprocessing.py
@@ -1,16 +1,10 @@
-import pandas as pd
 import re
 from nltk.corpus import stopwords
 
 from nltk.stem import WordNetLemmatizer
 
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
-
 REPLACE_BY_SPACE_RE = re.compile(r'[\-\(\)\{\}\[\]\/|@,;]')
-# BAD_SYMBOLS_RE = re.compile(r'[^a-z0-9_À-ÿ #+_]')
 BAD_SYMBOLS_RE = re.compile('[^a-zÀ-ÿ ]')
-# FRACTIONS_RE = re.compile('[^¼⅓½⅔¾]')
 STOPWORDS = set(stopwords.words('english'))
 MEASUREMENTS = re.compile(r'(teaspoons?)|( table[a-z]*)|(cups?)|(ounces?)|(pounds?)|(grams?)|(chopped)|(minced)|(fresh)|(min[a-z]*)')
 
@@ -20,7 +14,6 @@ def clean_text(text):
   text = REPLACE_BY_SPACE_RE.sub(' ', text)
   text = BAD_SYMBOLS_RE.sub('', text)
   text = MEASUREMENTS.sub('',text)
-  # text = 
   text = ' '.join(wnl.lemmatize(word) for word in text.split() if word not in STOPWORDS) # delete stopwors from text
   return text
 
